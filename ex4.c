b8b9842a95817ccfc8317dd11d8a508e9f74e4c3
andrelucas0982 <ribeiro.angeli111@gmail.com>
2025-03-19 19:48:53 -0300

    Add files via upload

---
+++ ex4.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main() {
+    float raio, diametro, circunferencia, area, pi = 3.14;
+
+    printf("Digite o raio do circulo: ");
+    scanf("%f", &raio);
+
+    diametro = 2 * raio;
+    circunferencia = 2 * pi * raio;
+    area = pi * raio * raio;
+
+    printf("Diametro: %.2f\n", diametro);
+    printf("Circunferencia: %.2f\n", circunferencia);
+    printf("Area: %.2f\n", area);
+
+    return 0;
+}
