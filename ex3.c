230038ef0889e7ae6db53d6769e94cf8af8943cb
andrelucas0982 <ribeiro.angeli111@gmail.com>
2025-03-19 10:55:48 -0300

    Update ex3.c

--- ex3.c
+++ ex3.c
@@ -2,25 +2,53 @@
 
 int main() {
 	
-	int A, B, C, Cateto;
-	
+	int A, B, C, CatetoMaior, Cateto1, Cateto2;
+
+    printf("Digite o 1 valor: ");
+	scanf("%d", &A);
+    
 	while(A != -1)
 	{
-		
-	printf("Digite o 1 valor: ");
-	scanf("%d", &A);
-	
     printf("Digite o 2 valor: ");
     scanf("%d", &B);
     
     printf("Digite o 3 valor: ");
     scanf("%d", &C);
-    
-    
+
+
+        if(A > B){
+            if(A > C){
+                
+                CatetoMaior = A;
+                Cateto1     = B;
+                Cateto2     = C;
+                
+            }
+        }else if(B > C){
+            if(B > A){
+                CatetoMaior = B;
+                Cateto1 = A;
+                Cateto2 = C;
+            }
+        } else {
+                CatetoMaior = C;
+                Cateto1     = A;
+                Cateto2     = B;
+        }
+
+
+        if((Cateto1 * Cateto1) + (Cateto2 * Cateto2) == (CatetoMaior * CatetoMaior) )
+            puts("Forma um triangulo retangulo.\n");
+        else
+            puts("Não forma um triangulo retangulo.\n");
+        
 	
-	
-}
+	printf("Digite o 1 valor: ");
+	scanf("%d", &A);
+    }
+    
   return 0;
 }
+
 
 
